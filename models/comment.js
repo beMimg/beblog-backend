60c1e21afd832dfd68f37959ec71d2663719f01e
beMimg <bernardommguerreiro@gmail.com>
2024-03-04 18:58:50 +0100

    models created

---
+++ models/comment.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+  text: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
---
+++ models/post.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  text: { type: String, required: true },
+  date: { type: Date, required: true },
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  isPublished: { type: Boolean, required: true },
+});
+
+PostSchema.virtual("url").get(function () {
+  return `/posts/${this._id}`;
+});
+
+module.exports = mongoose.model("Post", PostSchema);
