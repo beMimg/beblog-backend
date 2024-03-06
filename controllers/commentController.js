bcdf5d7da15b58af929a771bc03bdde602550cd0
beMimg <bernardommguerreiro@gmail.com>
2024-03-06 17:06:25 +0100

    post comment functional

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,2 +1,23 @@
 const mongoose = require("mongoose");
 const Comment = require("../models/comment");
+const Post = require("../models/post");
+
+exports.post_comment = async (req, res, next) => {
+  try {
+    const comment = new Comment({
+      author: req.user.user._id,
+      post: req.params.post_id,
+      text: req.body.text,
+    });
+    const existsPost = await Post.findById(req.params.post_id).exec();
+
+    if (!existsPost) {
+      return res.status(404).json({ message: "Post not found" });
+    } else {
+      await comment.save();
+      return res.status(200).json({ comment: comment });
+    }
+  } catch (err) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+};
--- routes/comment.js
+++ routes/comment.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
+const commentController = require("../controllers/commentController");
+const { authenticateToken } = require("../config/authenticate");
 
 router.get("/", (req, res, next) => {
   res.send(req.params.post_id);
 });
 
-router.post("/");
+router.post("/", authenticateToken, commentController.post_comment);
 
 module.exports = router;
