2b1f93c27267f37534589e60708fc6ebd637f465
beMimg <bernardommguerreiro@gmail.com>
2024-03-13 23:35:31 +0100

    change order of middleware

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ const limiter = RateLimit({
 });
 
 // view engine setup
+app.use(limiter);
+app.use(helmet());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 app.use(cors());
@@ -42,9 +44,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(helmet());
 app.use(compression());
-app.use(limiter);
 
 app.use("/api", indexRouter);
 app.use("/api/users", userRouter);
